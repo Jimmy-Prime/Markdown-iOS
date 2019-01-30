6c6694e1f0e0d0ec975243f2007e237344948ed9
Jimmy Lee <jimmylee@synology.com>
2019-01-30 11:12:49 +0800

    set up project

---
+++ Markdown-iOS/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Markdown-iOS
+//
+//  Created by Jimmy Lee on 2019/1/30.
+//  Copyright © 2019 Prime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+platform :ios, '11.0'
+
+target 'Markdown-iOS' do
+    use_frameworks!
+
+    pod 'SnapKit'
+    pod 'SwiftLint'
+
+    target 'Markdown-iOSTests' do
+        inherit! :search_paths
+    end
+
+    target 'Markdown-iOSUITests' do
+        inherit! :search_paths
+    end
+end
